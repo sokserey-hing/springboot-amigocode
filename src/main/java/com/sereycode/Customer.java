eebb25a0571e58023ad27ca0549c8da54e1a3cfe
sokserey-hing <soksereihing@gmail.com>
2023-10-05 22:06:21 +0700

    adding addCustomer and deleteCutomer to the API

--- src/main/java/com/sereycode/Customer.java
+++ src/main/java/com/sereycode/Customer.java
@@ -9,11 +9,13 @@ public class Customer {
     @Id
     @SequenceGenerator(
             name = "customer_id_sequence",
-            sequenceName = "customer_id_sequence"
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
             generator = "customer_id_sequence"
+
     )
     private Integer id;
     private String name;
